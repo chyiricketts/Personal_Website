3dc2b00b90927923a57b3564a5321377645c032a
Chyi Ricketts <chyi.ricketts@gmail.com>
2024-12-07 17:03:17 +0800

    Main website mostly finished, next step is data visualization page

--- main_script.js
+++ main_script.js
@@ -10,25 +10,3 @@ hoverZone.addEventListener('mouseenter', () => {
 navbar.addEventListener('mouseleave', () => {
     navbar.classList.remove('open'); // Hide navbar when leaving the navbar
 });
-
-
-
-
-
-
-//GET RID OF THIS IF IT DOESN'T DO ANYTHING
-//IS SUPPOSED TO BE FOR SMOOTH SCROLLING
-// Select all links with hashes
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent default anchor behavior
-
-        const target = document.querySelector(this.getAttribute('href'));
-
-        // Smoothly scroll to the target
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start' // Align at the top of the viewport
-        });
-    });
-});
