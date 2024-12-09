8ce556dbbbc12e860500f78b87b1fbe095f7ab06
Chyi Ricketts <chyi.ricketts@gmail.com>
2024-12-09 19:04:53 +0800

    navbar arrow toggle responsiveness

--- main_script.js
+++ main_script.js
@@ -2,11 +2,30 @@
 
 const navbar = document.querySelector('.navbar');
 const hoverZone = document.querySelector('.hover-zone');
+const arrowToggle = document.querySelector('.arrow-toggle');
+
+let isNavbarOpen = false;
 
 hoverZone.addEventListener('mouseenter', () => {
     navbar.classList.add('open');
+    arrowToggle.classList.add('active');
+    isNavbarOpen = true;
 });
 
 navbar.addEventListener('mouseleave', () => {
     navbar.classList.remove('open');
+    arrowToggle.classList.remove('active');
+    isNavbarOpen = false;
+});
+
+arrowToggle.addEventListener('click', () => {
+    isNavbarOpen = !isNavbarOpen;
+    if (isNavbarOpen) {
+        navbar.classList.add('open');
+        arrowToggle.classList.add('active');
+    }
+    else {
+        navbar.classList.remove('open');
+        arrowToggle.classList.remove('active');
+    }
 });
