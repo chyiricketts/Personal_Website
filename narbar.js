c214e4bdc32e391e53ef5006ce5cd01a5d0e0340
chyiricketts <chyi.ricketts@gmail.com>
2026-05-27 11:49:24 +0100

    Navigation bar update

--- narbar.js
+++ narbar.js
@@ -5,3 +5,11 @@ toggle.addEventListener("click", () => {
   navList.classList.toggle("active");
   toggle.classList.toggle("active");
 });
+
+
+const navToggle = document.getElementById("nav-toggle");
+const navLeft = document.querySelector(".nav-left");
+
+navToggle.addEventListener("click", () => {
+    navLeft.classList.toggle("active");
+});
