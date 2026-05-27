dba3b5103202576a6a35858dd059e0c1ccab5234
chyiricketts <chyi.ricketts@gmail.com>
2026-05-27 11:59:14 +0100

    Mobile navigation fix pt2

--- narbar.js
+++ narbar.js
@@ -1,12 +1,3 @@
-const toggle = document.getElementById("nav-toggle");
-const navList = document.querySelector(".nav-list");
-
-toggle.addEventListener("click", () => {
-  navList.classList.toggle("active");
-  toggle.classList.toggle("active");
-});
-
-
 const navToggle = document.getElementById("nav-toggle");
 const navLeft = document.querySelector(".nav-left");
 
