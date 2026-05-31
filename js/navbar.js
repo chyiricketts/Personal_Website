60fbf928f0693879baae5bcf487709ffd4441f40
chyiricketts <chyi.ricketts@gmail.com>
2026-05-31 20:20:29 +0100

    Integrating Certification Subsection

--- js/navbar.js
+++ js/navbar.js
@@ -4,3 +4,30 @@ const navLeft = document.querySelector(".nav-left");
 navToggle.addEventListener("click", () => {
     navLeft.classList.toggle("active");
 });
+
+
+let lastScrollY = window.scrollY;
+
+const navbar = document.querySelector(".navigation");
+
+window.addEventListener("scroll", () => {
+
+    // current position
+    const currentScrollY = window.scrollY;
+
+    // scrolling DOWN
+    if(currentScrollY > lastScrollY &&
+       currentScrollY > 80) {
+
+        navbar.classList.add("nav-hidden");
+    }
+
+    // scrolling UP
+    else {
+
+        navbar.classList.remove("nav-hidden");
+    }
+
+    // update last position
+    lastScrollY = currentScrollY;
+});
